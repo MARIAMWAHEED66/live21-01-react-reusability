d7376fd223d0616ba711ea7095ed533e4c714c23
mariamwaheed66 <m_7maid@hotmail.com>
2021-01-24 10:14:53 +0300

    pushed

--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,7 +1,16 @@
-const InstructorTag = () => {
-  // Create the InstructorTag component here
+import tags from "../data";
+import { Emoji, Name, GoToGithub, TagWrapper } from "../styles";
 
-  return <></>;
+import tags from "../tags";
+
+const InstructorTag = (props) => {
+  return (
+    <TagWrapper>
+      onClick=() => window.open{props.tag.link};<Emoji>{props.tag.emoji}</Emoji>
+      <Name>{props.tag.name}</Name>
+      <GoToGithub>Go to GitHub</GoToGithub>
+    </TagWrapper>
+  );
 };
 
 export default InstructorTag;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const tags = [
+    {
+      id: 1,
+      emoji: "🌚",
+      name: "Hamza",
+      Link: "https://github.com/DarthHamza";
+        
+    },
+
+    {
+        id: 2,
+        emoji: "🐥",
+        name: "Laila",
+        github: "https://github.com/Lailz";
+          
+      },
+      {
+        id: 3,
+        emoji: "🦍",
+        name: "Hasan",
+        github: " https://github.com/thehasanas";
+          
+      },
+    ];
+
+    export default tags;
+
